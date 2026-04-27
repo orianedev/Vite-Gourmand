16f3eb5f2ca119a160e6c18d9fa8bb2685ee9913
Orianedev <o.colandavaloo@gmail.com>
2026-04-27 19:13:03 +0200

    connexion database / projet

---
+++ includes/database.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "vite_gourmand";
+$user = "root";
+$pass = "root";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Erreur connexion : " . $e->getMessage());
+}
+?>
