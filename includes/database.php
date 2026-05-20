4c88f0b1555edf63c747916b4546733d4c0e7a74
Orianedev <o.colandavaloo@gmail.com>
2026-05-20 21:28:06 +0200

    modificztion de database pour déploiement

--- includes/database.php
+++ includes/database.php
@@ -1,13 +1,21 @@
 <?php
-$host = "localhost";
-$dbname = "vite_gourmand";
-$user = "root";
-$pass = "root";
+
+$host = "sql107.infinityfree.com";
+$dbname = "if0_41977457_vitegourmand_colandavaloo";
+$user = "if0_41977457";
+$pass = "Anbdy974";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass
+    );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch(PDOException $e) {
     die("Erreur connexion : " . $e->getMessage());
 }
+
 ?>
